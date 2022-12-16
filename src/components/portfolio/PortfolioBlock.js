d7bde60a659e43783b2511dd3628bc5c5c1a812b
JoeSangine <joesanginejr@gmail.com>
2022-12-16 08:51:59 -0500

    added

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -6,11 +6,7 @@ function PortfolioBlock(props) {
    const {image, live, source, title} = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-<<<<<<< HEAD
-         <Box component={'img'} src={image} alt={'mockup'}p={'0.4rem'}/>
-=======
          <Box component={'img'} src={image} alt={'mockup'} p={'0.4rem'}/>
->>>>>>> af54abd908c468d73b7ff394c3adf3eb2dc7e81a
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'3rem'}>
